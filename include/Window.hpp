7c1f75f28156436110cb64bd34ce86f382ba193d
Clément Lagier <clement.lagier@epitech.eu>
2024-07-11 18:02:46 +0400

    [UPDATE] Added three box, so i can put two buttons in a container

--- include/Window.hpp
+++ include/Window.hpp
@@ -2,14 +2,22 @@
 #include <iostream>
 #include <gtkmm.h>
 
+#define CSS_PROVIDER "../css/styles.css"
 
 class MyWindow : public Gtk::Window {
   public:
     MyWindow();
+    ~MyWindow();
 
   private:
     void on_button_clicked();
     bool on_key_pressed(GdkEventKey *event);
-    Gtk::Button m_button;
+    Gtk::Box _box;
+    Gtk::Box _box_bis;
+
+    Gtk::Box _container;
+    Gtk::Button _box_one_btn;
+    Gtk::Button _box_two_btn;
+
 };
 
